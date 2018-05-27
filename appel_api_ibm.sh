c7c9ac08f479b2ccc6401b48e0dd06f89fa439eb
Cagliostro <atfield2501@gmail.com>
2018-05-27 14:14:34 +0200

    //Script terminé

--- appel_api_ibm.sh
+++ appel_api_ibm.sh
@@ -3,10 +3,13 @@
 # Script de dialogue avec l'api ibm watson text-to-speech 
 # Auteur: Cagliostro <atfield2501@gmail.com>
 
+read -p "Entrez votre texte:" text
+read -p "Le nom du fichier de sortie (sans extension):" nom
+
 curl -X POST -u "******":"*****" \
 --header "Content-Type: application/json" \
---data "{\"text\":\"Nous aimons nous repaitres de ceux qui voudraient mous soumettres\"}" \
---output Addams_devise.mp3 \
+--data "{\"text\":\"$text\"}" \
+--output $nom.mp3 \
 "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?voice=fr-FR_ReneeVoice"
 
 
